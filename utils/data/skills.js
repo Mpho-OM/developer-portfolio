9ef1794958fe769470e38267fa9f93d6958e331d
Mpho-OM <Mpho.chuene2@oldmutual.com>
2025-07-15 09:43:28 +0200

    Update skills.js

--- utils/data/skills.js
+++ utils/data/skills.js
@@ -2,23 +2,17 @@ export const skillsData = [
   'HTML',
   'CSS',
   'Javascript',
-  'Typescript',
   'React',
-  'Next JS',
   'Tailwind',
-  'MongoDB',
   'MySQL',
   'PostgreSQL',
   'Git',
   'AWS',
+  'Python',
   'Bootstrap',
-  'Docker',
-  'Go',
   'Figma',
   'Firebase',
-  'MaterialUI',
-  'Nginx',
-  'Strapi'
+  'MaterialUI'
 ]
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
